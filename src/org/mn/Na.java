8bb93f2ae984b8edefc375ac9f21eeb33530e26f
jerovirat <jerovirat18@gmail.com>
2018-10-25 20:40:06 +0530

    create a file

--- src/org/mn/Na.java
+++ src/org/mn/Na.java
@@ -1,5 +1,12 @@
 package org.mn;
 
 public class Na {
+public static void main(String[] args) {
+	System.out.println("pout");
+}
+private void nana() {
+	// TODO Auto-generated method stub
+	System.out.println("poiuytre");
 
+}
 }
